81f4e191aeb081c9e1e4410331adbefb21839bd8
Kavya <kavyagp32@gmail.com>
2026-07-30 12:47:13 +0530

    Day1

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+        System.out.println("welcome to the java track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now I am learning how to eexpress that logic using Java");
+ 
     }
 }
