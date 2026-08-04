ef17cbd7f3b9e164aeb0df6f45419d7af12b8dd9
Kavya <kavyagp32@gmail.com>
2026-08-04 12:29:33 +0530

    Day3

---
+++ track/M02-Intr-to-java/T02/Prg2.java
@@ -0,0 +1,14 @@
+
+public class Prg2 {
+
+    public static void main(String[] args) {
+        int a = -98;
+        if (a > 0) {
+            System.out.println("Positive");
+        } else if (a < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
