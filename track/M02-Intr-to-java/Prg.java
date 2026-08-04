9741601637934b3491b251fd19a7cb2a9913e5b2
kavya-16-ux <kavyagp32@gmail.com>
2026-08-04 11:49:31 +0530

    Remove Prg.java class and main method
    Signed-off-by: kavya-16-ux <kavyagp32@gmail.com>

--- track/M02-Intr-to-java/Prg.java
+++ track/M02-Intr-to-java/Prg.java
@@ -1,13 +1,1 @@
 
-public class Prg {
-
-    public static void main(String[] args) {
-        System.out.println("Monday");
-        System.out.print("Tuesday");
-        System.out.println("Wednesday");
-        System.err.println("Thursday");
-        System.out.print("Friday");
-        System.err.println("Saturday");
-        System.err.print("Sunday");
-    }
-}
