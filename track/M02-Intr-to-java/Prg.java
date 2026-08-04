75bd50b0fd786c97a8fce143f2235c113c77bf7c
Kavya <kavyagp32@gmail.com>
2026-08-04 11:36:14 +0530

    Day3

---
+++ track/M02-Intr-to-java/Prg.java
@@ -0,0 +1,13 @@
+
+public class Prg {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.err.println("Thursday");
+        System.out.print("Friday");
+        System.err.println("Saturday");
+        System.err.print("Sunday");
+    }
+}
