9b72ca634a3cec0d9dbf87092dd1765dd0e1e767
Kavya <kavyagp32@gmail.com>
2026-09-07 17:23:40 +0530

    StringProbelms

---
+++ track/M04-Strings/T01/P01.java
@@ -0,0 +1,13 @@
+
+class P01 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        if (s1 == s2) {
+            System.out.println("reference are equal");
+        } else {
+            System.out.println("reference are not equal");
+        }
+    }
+}
---
+++ track/M04-Strings/T01/P02.java
@@ -0,0 +1,13 @@
+
+class P02 {
+
+    public static void main(String[] args) {
+        String s1 = new String("Java");
+        String s2 = new String("Java");
+        if (s1 == s2) {
+            System.out.println("reference are equal");
+        } else {
+            System.out.println("reference are not equal");
+        }
+    }
+}
