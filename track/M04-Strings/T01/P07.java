80fc6dfe52f9d76784f4d857f2d1bc05957f19b5
Kavya <kavyagp32@gmail.com>
2026-09-08 17:35:57 +0530

    StringProbelms

---
+++ track/M04-Strings/T01/P07.java
@@ -0,0 +1,20 @@
+
+public class P07 {
+
+    public static void main(String[] args) {
+        String s1 = "KodNest Technologies";
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.contains("Nest"));
+        System.out.println(s1.contains("nest"));
+        System.out.println(s1.startsWith("Kod"));
+        System.out.println(s1.endsWith("ies"));
+        System.out.println(s1.indexOf('e'));//first e index
+        System.out.println(s1.length());
+        System.out.println(s1.substring(5));//from index 5 to end
+        System.out.println(s1.substring(5, 14));//from index 5 to 13
+        System.out.println(s1.replace('K', 'E'));
+
+    }
+}
---
+++ track/M04-Strings/T01/Prg1.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Prg1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstText = scanner.nextLine();
+        String secondText = scanner.nextLine();
+
+        // Compare the two values in both ways.
+        boolean exactMatch = firstText.equals(secondText);
+        boolean ignoreCase = firstText.equalsIgnoreCase(secondText);
+        System.out.println("Exact match: " + exactMatch);
+        System.out.println("Ignore-case match: " + ignoreCase);
+    }
+}
