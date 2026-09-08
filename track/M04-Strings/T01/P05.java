d68d52d76c03832b32b5ae2f0414a7d7aabc13f9
Kavya <kavyagp32@gmail.com>
2026-09-08 16:50:23 +0530

    StringProblems

---
+++ track/M04-Strings/T01/P05.java
@@ -0,0 +1,10 @@
+
+class P05 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        for (int i = 0; i <= s1.length() - 1; i++) {
+            System.out.println(s1.charAt(i));
+        }
+    }
+}
---
+++ track/M04-Strings/T01/P06.java
@@ -0,0 +1,18 @@
+
+class P06 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "JaVa";
+        if (s1 == s2) {
+            System.out.println("reference are equal");
+        } else {
+            System.out.println("reference are not equal");
+        }
+        if (s1.equalsIgnoreCase(s2)) {
+            System.out.println("Strings are same");
+        } else {
+            System.out.println("Strings are not same");
+        }
+    }
+}
